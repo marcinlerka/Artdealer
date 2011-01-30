ffda3109a00c1b0426cb55bb251c96de538944ae
ML <marcinlerka@gmail.com>
2011-01-30 21:19:05 +0100

    Some cosmetic changes.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,8 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   #    ==   CanCan exceptions logging  ==  
-      rescue_from CanCan::AccessDenied do |exception|
-        Rails.logger.debug "Access denied on #{exception.action} #{exception.subject.inspect}"
-      end
+  #    rescue_from CanCan::AccessDenied do |exception|
+  #      Rails.logger.debug "Access denied on #{exception.action} #{exception.subject.inspect}"
+  #    end
 
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,7 @@ module ApplicationHelper
     link_to(content, object) if can?(:read, object)
   end
 
+  def users_link(object, content)
+    link_to(content, users_path) if can?(:read, object)
+  end
 end
